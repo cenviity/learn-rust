3e97c79c495c9d97fc01ae5ad444907ec9bde1da
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-03-17 14:54:21 +1100

    Return ownership of strings in `greet`

--- 04/references_and_borrowing/src/main.rs
+++ 04/references_and_borrowing/src/main.rs
@@ -1,10 +1,11 @@
 fn main() {
     let m1 = String::from("Hello");
     let m2 = String::from("world");
-    greet(m1, m2);
-    let s = format!("{} {}", m1, m2);
+    let (m1_again, m2_again) = greet(m1, m2);
+    let s = format!("{} {}", m1_again, m2_again);
 }
 
-fn greet(g1: String, g2: String) {
+fn greet(g1: String, g2: String) -> (String, String) {
     println!("{} {}!", g1, g2);
+    (g1, g2)
 }
