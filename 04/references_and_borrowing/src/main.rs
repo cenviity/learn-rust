5490aaf6847ffa02e49d36df4c2e5f00e01c3adc
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-03-17 15:44:03 +1100

    Demonstrate immutable references

--- 04/references_and_borrowing/src/main.rs
+++ 04/references_and_borrowing/src/main.rs
@@ -1,10 +1,8 @@
 fn main() {
-    let m1 = String::from("Hello");
-    let m2 = String::from("world");
-    greet(&m1, &m2);
-    let s = format!("{} {}", m1, m2);
-}
+    let mut v: Vec<i32> = vec![1, 2, 3];
 
-fn greet(g1: &String, g2: &String) {
-    println!("{} {}!", g1, g2);
+    let num: &i32 = &v[2];
+
+    v.push(4);
+    println!("Third element is: {}", *num);
 }
