6279c0bfeb526129ff68e36e1d6b1d83bf284f15
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-03-18 22:30:50 +1100

    Add examples of `String`s and string slices

--- 04/slice_type/src/main.rs
+++ 04/slice_type/src/main.rs
@@ -1,11 +1,26 @@
-fn main() {
-    let mut s = String::from("hello world");
+#[allow(unused_variables)]
 
-    let word = first_word(&s);
+fn main() {
+    let my_string = String::from("hello world");
+
+    // `first_word` works on slices of `String`s, whether partial or whole
+    let word = first_word(&my_string[0..6]);
+    let word = first_word(&my_string[..]);
+    // `first_word` also works on references to `String`s, which are equivalent
+    // to whole slices of `String`s
+    let word = first_word(&my_string);
+
+    let my_string_literal = "hello world";
+
+    // `first_word` works on slices of string literals, whether partial or whole
+    let word = first_word(&my_string_literal[0..6]);
+    let word = first_word(&my_string_literal[..]);
+
+    // Because string literals *are* string slices already,
+    // this works too, without the slice syntax!
+    let word = first_word(&my_string_literal);
 
     println!("the first word is: {}", word);
-
-    s.clear();
 }
 
 fn first_word(s: &str) -> &str {
