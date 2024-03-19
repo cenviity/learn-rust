d35535593bc3f296b083a3083cb4a2fd5dbe910c
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-03-20 03:13:16 +1100

    Add `width` method

--- 05/example-using-structs/src/main.rs
+++ 05/example-using-structs/src/main.rs
@@ -8,6 +8,10 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
 }
 
 fn main() {
@@ -16,8 +20,7 @@ fn main() {
         height: 50,
     };
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        rect1.area()
-    );
+    if rect1.width() {
+        println!("The rectangle has a nonzero width; it is {}", rect1.width);
+    }
 }
