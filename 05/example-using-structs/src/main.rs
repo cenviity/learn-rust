3662c079a0a07885e0c7e931b9f70b510d764caf
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-03-20 03:26:05 +1100

    Add `can_hold` method

--- 05/example-using-structs/src/main.rs
+++ 05/example-using-structs/src/main.rs
@@ -12,6 +12,10 @@ impl Rectangle {
     fn width(&self) -> bool {
         self.width > 0
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 
 fn main() {
@@ -19,8 +23,15 @@ fn main() {
         width: 30,
         height: 50,
     };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
 
-    if rect1.width() {
-        println!("The rectangle has a nonzero width; it is {}", rect1.width);
-    }
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
