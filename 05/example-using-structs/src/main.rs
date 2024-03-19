900ceaa8fb35a108f43a097659beedecded7cf2a
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-03-20 03:38:14 +1100

    Demonstrate methods are syntactic sugar for functions

--- 05/example-using-structs/src/main.rs
+++ 05/example-using-structs/src/main.rs
@@ -45,4 +45,15 @@ fn main() {
 
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let mut r = Rectangle {
+        width: 1,
+        height: 2,
+    };
+    let area1 = r.area();
+    let area2 = Rectangle::area(&r);
+    assert_eq!(area1, area2);
+
+    r.set_width(2);
+    Rectangle::set_width(&mut r, 2);
 }
