11eab9cef2c8bbfb4bec943d21c323017c4bbe1b
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-03-20 20:55:17 +1100

    Add `IpAddr` struct

--- 06/defining-enums/src/main.rs
+++ 06/defining-enums/src/main.rs
@@ -1,14 +1,21 @@
-enum IpAddrKind {
-    V4,
-    V6,
-}
-
 fn main() {
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
+    enum IpAddrKind {
+        V4,
+        V6,
+    }
 
-    route(IpAddrKind::V4);
-    route(IpAddrKind::V6);
+    struct IpAddr {
+        kind: IpAddrKind,
+        address: String,
+    }
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
 }
-
-fn route(ip_kind: IpAddrKind) {}
