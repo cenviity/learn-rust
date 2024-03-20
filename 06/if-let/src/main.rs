c3bdaeeeabe0b6546c3913b9311f7befdbaa04ce
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-03-20 22:23:18 +1100

    Demonstrate using `match` expression with `Coin` enum

--- 06/if-let/src/main.rs
+++ 06/if-let/src/main.rs
@@ -1,6 +1,21 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
 fn main() {
-    let config_max = Some(3u8);
-    if let Some(max) = config_max {
-        println!("The maximum is configured to be {}", max)
+    let coin = Coin::Penny;
+    let mut count = 0;
+    match coin {
+        Coin::Quarter(state) => println!("State quarter from {:?}", state),
+        _ => count += 1,
     }
 }
