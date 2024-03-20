baa5af97c869c5227d51b75401470dd5d5423af6
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-03-20 21:00:32 +1100

    Demonstrate simplified standard library definition

--- 06/defining-enums/src/main.rs
+++ 06/defining-enums/src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
+    struct Ipv4Addr {}
+
+    struct Ipv6Addr {}
+
     enum IpAddr {
-        V4(u8, u8, u8, u8),
-        V6(String),
+        V4(Ipv4Addr),
+        V6(Ipv6Addr),
     }
-
-    let home = IpAddr::V4(127, 0, 0, 1);
-
-    let loopback = IpAddr::V6(String::from("::1"));
 }
