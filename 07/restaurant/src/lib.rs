4d61f422b9dbc1bb0efe51c8e6edd2442f6ee405
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-03-22 02:21:51 +1100

    Extract `front_of_house` module

--- 07/restaurant/src/lib.rs
+++ 07/restaurant/src/lib.rs
@@ -1,20 +1,6 @@
 pub use crate::front_of_house::hosting;
 
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
+mod front_of_house;
 
 mod back_of_house {
     pub struct Breakfast {
