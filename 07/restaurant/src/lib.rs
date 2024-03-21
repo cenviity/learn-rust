50aca3eb831c8ddb0ef944be9c87eee0d1c84938
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-03-22 01:35:10 +1100

    Add `Appetizer` enum

--- 07/restaurant/src/lib.rs
+++ 07/restaurant/src/lib.rs
@@ -29,6 +29,11 @@ mod back_of_house {
         }
     }
 
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
     fn fix_incorrect_order() {
         cook_order();
         super::deliver_order();
@@ -47,6 +52,9 @@ pub fn eat_at_restaurant() {
     // The next line won't compile if we uncomment it; we're not allowed
     // to see or modify the seasonal fruit that comes with the meal
     // meal.seasonal_fruit = String::from("blueberries");
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
 }
 
 fn deliver_order() {}
