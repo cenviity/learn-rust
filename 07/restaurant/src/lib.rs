e3712716ecaaebcf1abc2a51379a3ace4cbe99ee
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-03-22 02:02:16 +1100

    Revert "Move `use` inside `customer` module to fix errors"
    This reverts commit da13224974024a6e2ffdd510a4da3bdff1429106.

--- 07/restaurant/src/lib.rs
+++ 07/restaurant/src/lib.rs
@@ -1,3 +1,5 @@
+use crate::front_of_house::hosting;
+
 mod front_of_house {
     pub mod hosting {
         pub fn add_to_waitlist() {}
@@ -43,9 +45,6 @@ mod back_of_house {
 }
 
 mod customer {
-    use crate::back_of_house;
-    use crate::front_of_house::hosting;
-
     pub fn eat_at_restaurant() {
         hosting::add_to_waitlist();
 
