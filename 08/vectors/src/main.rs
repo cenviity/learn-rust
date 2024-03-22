8ed02d63029a4dc719455ebff228306c4de416ad
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-03-22 22:46:29 +1100

    Try to mutate a vector while holding an immutable reference to it

--- 08/vectors/src/main.rs
+++ 08/vectors/src/main.rs
@@ -1,11 +1,7 @@
-use std::slice::Iter;
+fn main() {}
 
-fn main() {
-    let mut v: Vec<i32> = vec![1, 2];
-
-    let mut iter: Iter<'_, i32> = v.iter();
-
-    let n1: &i32 = iter.next().unwrap();
-    let n2: &i32 = iter.next().unwrap();
-    let end: Option<&i32> = iter.next();
+fn dup_in_place(v: &mut Vec<i32>) {
+    for n_ref in v.iter() {
+        v.push(*n_ref);
+    }
 }
