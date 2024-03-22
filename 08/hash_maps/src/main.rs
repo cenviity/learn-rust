291bbcdba4699bb467d4d8a1ba211dc504957d8d
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-03-23 00:06:33 +1100

    Try using values after they have been inserted into a hash map

--- 08/hash_maps/src/main.rs
+++ 08/hash_maps/src/main.rs
@@ -1,12 +1,13 @@
 use std::collections::HashMap;
 
 fn main() {
-    let mut scores = HashMap::new();
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
 
-    scores.insert(String::from("Blue"), 10);
-    scores.insert(String::from("Yellow"), 50);
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+    // field_name and field_value are invalid at this point
+    // Try using them and see what compiler error you get!
 
-    for (key, value) in &scores {
-        println!("{key}: {value}");
-    }
+    println!("{field_name}: {field_value}");
 }
