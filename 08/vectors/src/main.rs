b74d91c066f8b526af09a741d5b93fbadb97b07d
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-03-22 22:52:48 +1100

    Use an enum in a vector

--- 08/vectors/src/main.rs
+++ 08/vectors/src/main.rs
@@ -1,8 +1,13 @@
-use std::ops::Range;
-
 fn main() {
-    let mut v: Vec<i32> = vec![1, 2];
-    let mut iter: Range<usize> = 0..v.len();
-    let i1: usize = iter.next().unwrap();
-    let n1: &i32 = &v[i1];
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
 }
