8f2f18d6606c8cff986ffa5b54b8fd7d97ce6448
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-03-23 00:09:09 +1100

    Overwrite a value in a hash map

--- 08/hash_maps/src/main.rs
+++ 08/hash_maps/src/main.rs
@@ -1,13 +1,10 @@
 use std::collections::HashMap;
 
 fn main() {
-    let field_name = String::from("Favorite color");
-    let field_value = String::from("Blue");
+    let mut scores = HashMap::new();
 
-    let mut map = HashMap::new();
-    map.insert(field_name, field_value);
-    // field_name and field_value are invalid at this point
-    // Try using them and see what compiler error you get!
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Blue"), 25);
 
-    println!("{field_name}: {field_value}");
+    println!("{:?}", scores);
 }
