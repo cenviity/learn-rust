64bb588302d3a9dd4ac94cd64979568013765d59
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-03-22 23:13:00 +1100

    Create some strings

--- 08/strings/src/main.rs
+++ 08/strings/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::new();
+
+    let data = "initial contents";
+
+    let s = data.to_string();
+
+    // the method also works on a literal directly:
+    let s = "initial contents".to_string();
+
+    let s = String::from("initial contents");
 }
