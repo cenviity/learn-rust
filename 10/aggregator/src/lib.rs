71a633458f71d50e96a84a05cd9d03d89c6c65b3
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-03-24 01:17:54 +1100

    Use two parameters implementing `Summary` that may be different types

--- 10/aggregator/src/lib.rs
+++ 10/aggregator/src/lib.rs
@@ -27,6 +27,6 @@ impl Summary for Tweet {
     }
 }
 
-pub fn notify<T: Summary>(item: &T) {
-    println!("Breaking news! {}", item.summarize());
+pub fn notify(item1: &impl Summary, item2: &impl Summary) {
+    println!("Breaking news! {}", item1.summarize());
 }
