e143591bbbf52f1433dd21b62d4e088af3574d37
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-03-24 01:01:34 +1100

    Add `Summary::summarize_author` and call it from `summarize`

--- 10/aggregator/src/lib.rs
+++ 10/aggregator/src/lib.rs
@@ -1,17 +1,10 @@
 pub trait Summary {
+    fn summarize_author(&self) -> String;
+
     fn summarize(&self) -> String {
-        String::from("(Read more...)")
+        format!("(Read more from {}...)", self.summarize_author())
     }
 }
-
-pub struct NewsArticle {
-    pub headline: String,
-    pub location: String,
-    pub author: String,
-    pub content: String,
-}
-
-impl Summary for NewsArticle {}
 
 pub struct Tweet {
     pub username: String,
@@ -21,7 +14,7 @@ pub struct Tweet {
 }
 
 impl Summary for Tweet {
-    fn summarize(&self) -> String {
-        format!("{}: {}", self.username, self.content)
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
     }
 }
