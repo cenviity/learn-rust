2c749aa107597a0dee8078c1347239d1189fda59
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-03-23 17:21:57 +1100

    Simplify even further with `read_to_string` function

--- 09/result/src/main.rs
+++ 09/result/src/main.rs
@@ -1,12 +1,7 @@
-use std::{
-    fs::File,
-    io::{self, Read},
-};
+use std::{fs, io};
 
 fn main() {}
 
 fn read_username_from_file() -> Result<String, io::Error> {
-    let mut username = String::new();
-    File::open("hello.txt")?.read_to_string(&mut username)?;
-    Ok(username)
+    fs::read_to_string("hello.txt")
 }
