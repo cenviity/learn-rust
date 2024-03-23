0d10f5b52ff81820815a9d7b95c24c0265029ca0
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-03-24 01:10:56 +1100

    Add `notify` function generic over types that implement `Summary`

--- 10/aggregator/src/lib.rs
+++ 10/aggregator/src/lib.rs
@@ -1,8 +1,16 @@
 pub trait Summary {
-    fn summarize_author(&self) -> String;
+    fn summarize(&self) -> String;
+}
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
 
+impl Summary for NewsArticle {
     fn summarize(&self) -> String {
-        format!("(Read more from {}...)", self.summarize_author())
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
     }
 }
 
@@ -14,7 +22,11 @@ pub struct Tweet {
 }
 
 impl Summary for Tweet {
-    fn summarize_author(&self) -> String {
-        format!("@{}", self.username)
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
     }
+}
+
+pub fn notify(item: &impl Summary) {
+    println!("Breaking news! {}", item.summarize());
 }
