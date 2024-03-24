c3bf692fd1cbdbe787235dfd02873c4e62bff7ac
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-03-24 22:06:16 +1100

    Add `ImportantExcerpt` struct holding reference to a string slice

--- 10/lifetimes/src/main.rs
+++ 10/lifetimes/src/main.rs
@@ -1,12 +1,11 @@
-fn main() {
-    let string1 = String::from("long string is long");
-    let string2 = String::from("xyz");
-
-    let result = longest(string1.as_str(), string2.as_str());
-    println!("The longest string is {}", result);
+struct ImportantExcerpt<'a> {
+    part: &'a str,
 }
 
-fn longest<'a>(x: &str, y: &str) -> &'a str {
-    let result = String::from("really long string");
-    result.as_str()
+fn main() {
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
 }
