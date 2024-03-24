18b14e2ee64dcb491a49c71143b6f6100d59b0a0
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-03-24 22:26:25 +1100

    Add another method to demonstrate third lifetime elision rule

--- 10/lifetimes/src/main.rs
+++ 10/lifetimes/src/main.rs
@@ -6,6 +6,11 @@ impl<'a> ImportantExcerpt<'a> {
     fn level(&self) -> i32 {
         3
     }
+
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {}", announcement);
+        self.part
+    }
 }
 
 fn main() {
