0df97c3527674b5a09cef57142fa330b6f7e5c0c
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-03-24 22:23:29 +1100

    Add `ImportantExcerpt::level()` to demonstrate first lifetime elision rule

--- 10/lifetimes/src/main.rs
+++ 10/lifetimes/src/main.rs
@@ -2,6 +2,12 @@ struct ImportantExcerpt<'a> {
     part: &'a str,
 }
 
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+}
+
 fn main() {
     let novel = String::from("Call me Ishmael. Some years ago...");
     let first_sentence = novel.split('.').next().expect("Could not find a '.'");
