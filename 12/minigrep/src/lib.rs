c8241f04e16a9b33cb79a9e2eff75ab8e72fb8b0
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-03-25 16:15:24 +1100

    Add failing `case_insensitive` test and update original test

--- 12/minigrep/src/lib.rs
+++ 12/minigrep/src/lib.rs
@@ -45,13 +45,29 @@ mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
             Rust:\n\
             safe, fast, productive.\n\
-            Pick three.";
+            Pick three.\n\
+            Duct tape.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+        Rust:\n\
+        safe, fast, productive.\n\
+        Pick three.\n\
+        Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
