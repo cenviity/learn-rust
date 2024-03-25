390fe057a9ca4eca6b2a102efa5262ea960b3def
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-03-25 15:19:23 +1100

    Remove `println` statements

--- 12/minigrep/src/lib.rs
+++ 12/minigrep/src/lib.rs
@@ -19,9 +19,7 @@ impl Config {
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n{contents}");
+    let _contents = fs::read_to_string(config.file_path)?;
 
     Ok(())
 }
