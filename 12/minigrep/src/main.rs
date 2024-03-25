1c58b214dbd74d38dc29551650e73cdd9d0b791a
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-03-25 12:12:12 +1100

    Rename `Config::new` to `build` and return `Result` instead

--- 12/minigrep/src/main.rs
+++ 12/minigrep/src/main.rs
@@ -3,7 +3,7 @@ use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::build(&args);
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -20,14 +20,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Self {
+    fn build(args: &[String]) -> Result<Self, &'static str> {
         if args.len() < 3 {
-            panic!("not enough arguments");
+            return Err("not enough arguments");
         }
 
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Self { query, file_path }
+        Ok(Self { query, file_path })
     }
 }
