1377f0ed20a3ca039a6959f92b280abb24b66f90
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-03-26 21:46:53 +1100

    Demonstrate `sort_by_key` with another `FnMut` closure

--- 13/closures/src/main.rs
+++ 13/closures/src/main.rs
@@ -20,12 +20,10 @@ fn main() {
         },
     ];
 
-    let mut sort_operations = vec![];
-    let value = String::from("by key called");
-
+    let mut num_sort_operations = 0;
     list.sort_by_key(|r| {
-        sort_operations.push(value);
+        num_sort_operations += 1;
         r.width
     });
-    println!("{:#?}", list);
+    println!("{:#?}, sorted in {num_sort_operations} operations", list);
 }
