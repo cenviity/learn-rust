9a65a91b76bf28047cbda6f90d8eccb146b95bd3
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-03-26 22:12:23 +1100

    Remove `drop` call

--- 13/closures/src/main.rs
+++ 13/closures/src/main.rs
@@ -5,6 +5,6 @@ fn make_a_cloner(s_ref: &str) -> impl Fn() -> String + '_ {
 fn main() {
     let s_own = String::from("Hello world");
     let cloner = make_a_cloner(&s_own);
-    drop(s_own);
+    // drop(s_own);
     cloner();
 }
