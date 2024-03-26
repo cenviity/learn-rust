b5e41624c4b813cb068e688f43e011fb1552b627
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-03-26 17:34:33 +1100

    Move ownership into closure

--- 13/closures/src/main.rs
+++ 13/closures/src/main.rs
@@ -1,9 +1,10 @@
+use std::thread;
+
 fn main() {
-    let mut list = vec![1, 2, 3];
+    let list = vec![1, 2, 3];
     println!("Before defining closure, {:?}", list);
 
-    let mut borrows_mutably = || list.push(7);
-
-    borrows_mutably();
-    println!("After calling closure: {:?}", list);
+    thread::spawn(move || println!("From thread: {:?}", list))
+        .join()
+        .unwrap();
 }
