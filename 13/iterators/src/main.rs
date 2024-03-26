5d0d0e145c35b1e81f8fea482fa36f52d32f5575
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-03-26 23:54:52 +1100

    Demonstrate `Iterator::next`

--- 13/iterators/src/main.rs
+++ 13/iterators/src/main.rs
@@ -1,9 +1,16 @@
-fn main() {
-    let v1 = vec![1, 2, 3];
+fn main() {}
 
-    let v1_iter = v1.iter();
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn iterator_demonstration() {
+        let v1 = vec![1, 2, 3];
 
-    for val in v1_iter {
-        println!("Got: {}", val);
+        let mut v1_iter = v1.iter();
+
+        assert_eq!(v1_iter.next(), Some(&1));
+        assert_eq!(v1_iter.next(), Some(&2));
+        assert_eq!(v1_iter.next(), Some(&3));
+        assert_eq!(v1_iter.next(), None);
     }
 }
