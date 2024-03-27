0627ee6d75fdb5c0db64c88aa08fcc43d34209fe
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-03-27 14:03:55 +1100

    Suppress warnings

--- 13/iterators/src/main.rs
+++ 13/iterators/src/main.rs
@@ -4,6 +4,7 @@ struct Shoe {
     style: String,
 }
 
+#[allow(dead_code)]
 fn shoes_in_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
     shoes.into_iter().filter(|s| s.size == shoe_size).collect()
 }
