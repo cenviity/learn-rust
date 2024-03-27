230fe5e54066ff1396c19c71046eacd68468e9a6
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-03-27 21:51:36 +1100

    Add example modules

--- 14/art/src/lib.rs
+++ 14/art/src/lib.rs
@@ -1,14 +1,29 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+//! # Art
+//!
+//! A library for modeling artistic concepts.
+
+pub mod kinds {
+    /// The primary colors according to the RYB color model.
+    pub enum PrimaryColor {
+        Red,
+        Yellow,
+        Blue,
+    }
+
+    /// The secondary colors according to the RYB color model.
+    pub enum SecondaryColor {
+        Orange,
+        Green,
+        Purple,
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub mod utils {
+    use crate::kinds::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    /// Combines two primary colors in equal amounts to create
+    /// a secondary color.
+    pub fn mix(c1: PrimaryColor, c2: PrimaryColor) -> SecondaryColor {
+        unimplemented!()
     }
 }
