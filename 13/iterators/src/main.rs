f89bb188dfe121fca2410b385e45e83597b3fb9a
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-03-27 12:44:16 +1100

    Demonstrate `filter` iterator

--- 13/iterators/src/main.rs
+++ 13/iterators/src/main.rs
@@ -1,7 +1,50 @@
-fn main() {
-    let v1: Vec<i32> = vec![1, 2, 3];
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
+}
 
-    let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+fn shoes_in_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+}
 
-    assert_eq!(v2, vec![2, 3, 4]);
+fn main() {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn filters_by_size() {
+        let shoes = vec![
+            Shoe {
+                size: 10,
+                style: String::from("sneaker"),
+            },
+            Shoe {
+                size: 13,
+                style: String::from("sandal"),
+            },
+            Shoe {
+                size: 10,
+                style: String::from("boot"),
+            },
+        ];
+
+        let in_my_size = shoes_in_size(shoes, 10);
+
+        assert_eq!(
+            in_my_size,
+            vec![
+                Shoe {
+                    size: 10,
+                    style: String::from("sneaker")
+                },
+                Shoe {
+                    size: 10,
+                    style: String::from("boot")
+                },
+            ]
+        );
+    }
 }
