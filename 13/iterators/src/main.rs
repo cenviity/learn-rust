4f6c8955cfaead7b06dd3220259992186959119c
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-03-27 12:33:29 +1100

    Demonstrate lazy `map` iterator

--- 13/iterators/src/main.rs
+++ 13/iterators/src/main.rs
@@ -1,15 +1,5 @@
-fn main() {}
+fn main() {
+    let v1: Vec<i32> = vec![1, 2, 3];
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn iterator_sum() {
-        let v1 = vec![1, 2, 3];
-
-        let v1_iter = v1.iter();
-
-        let total: i32 = v1_iter.sum();
-
-        assert_eq!(total, 6);
-    }
+    v1.iter().map(|x| x + 1);
 }
