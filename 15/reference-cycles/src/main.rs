49d16f3f31cca1c8afcdf56c3808771e0ee81419
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-04-02 16:54:33 +1100

    Add `List` type with `tail` method

--- 15/reference-cycles/src/main.rs
+++ 15/reference-cycles/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use crate::List::{Cons, Nil};
+use std::{cell::RefCell, rc::Rc};
+
+#[derive(Debug)]
+enum List {
+    Cons(i32, RefCell<Rc<List>>),
+    Nil,
 }
+
+impl List {
+    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+        match self {
+            Cons(_, item) => Some(item),
+            Nil => None,
+        }
+    }
+}
+
+fn main() {}
