65651aa35991bb9a7d422801ecc592c06c200cc3
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-04-06 21:11:40 +1100

    Add `leaf` and `branch`

--- 15/reference-cycles/src/main.rs
+++ 15/reference-cycles/src/main.rs
@@ -6,4 +6,13 @@ struct Node {
     children: RefCell<Vec<Rc<Node>>>,
 }
 
-fn main() {}
+fn main() {
+    let leaf = Rc::new(Node {
+        value: 3,
+        children: RefCell::new(vec![]),
+    });
+    let branch = Rc::new(Node {
+        value: 5,
+        children: RefCell::new(vec![Rc::clone(&leaf)]),
+    });
+}
