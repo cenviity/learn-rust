4cbf95261fa1e4d5ab04784636c7eb1731b15087
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-04-07 08:50:20 +1000

    Send a string from a spawned thread

--- 16/message-passing/src/main.rs
+++ 16/message-passing/src/main.rs
@@ -1,5 +1,10 @@
-use std::sync::mpsc;
+use std::{sync::mpsc, thread};
 
 fn main() {
     let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+    });
 }
