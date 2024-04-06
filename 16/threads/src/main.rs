d4b9b86539772f329141d5ca3aa6209fc2fa431a
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-04-06 23:38:37 +1100

    Try to use vector from main thread in spawned thread

--- 16/threads/src/main.rs
+++ 16/threads/src/main.rs
@@ -1,17 +1,11 @@
-use std::{thread, time::Duration};
+use std::thread;
 
 fn main() {
+    let v = vec![1, 2, 3];
+
     let handle = thread::spawn(|| {
-        for i in 1..10 {
-            println!("hi number {} from the spawned thread!", i);
-            thread::sleep(Duration::from_millis(1));
-        }
+        println!("Here's a vector: {:?}", v);
     });
 
     handle.join().unwrap();
-
-    for i in 1..5 {
-        println!("hi number {} from the main thread!", i);
-        thread::sleep(Duration::from_millis(1));
-    }
 }
