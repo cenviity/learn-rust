ab143c96f52135390d60d05ef74ce7de640c636f
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-04-07 23:22:18 +1000

    Try to use `..` more than once in a tuple pattern

--- 18/pattern-syntax/src/main.rs
+++ 18/pattern-syntax/src/main.rs
@@ -2,8 +2,8 @@ fn main() {
     let numbers = (2, 4, 8, 16, 32);
 
     match numbers {
-        (first, .., last) => {
-            println!("Some numbers: {first}, {last}");
+        (.., second, ..) => {
+            println!("Some numbers: {second}");
         }
     }
 }
