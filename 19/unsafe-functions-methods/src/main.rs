7574b1b0b1ab240b22a3aa7ece8fa460499d8e86
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-04-08 00:47:42 +1000

    Declare and call an `extern` function defined in C

--- 19/unsafe-functions-methods/src/main.rs
+++ 19/unsafe-functions-methods/src/main.rs
@@ -1,8 +1,9 @@
-use std::slice;
+extern "C" {
+    fn abs(input: i32) -> i32;
+}
 
 fn main() {
-    let address = 0x01234usize;
-    let r = address as *mut i32;
-
-    let values: &[i32] = unsafe { slice::from_raw_parts_mut(r, 10000) };
+    unsafe {
+        println!("Absolute value of -3 according to C: {}", abs(-3));
+    }
 }
