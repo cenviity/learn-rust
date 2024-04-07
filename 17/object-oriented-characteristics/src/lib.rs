033601ff0a9dc479c4ebefbe0fd2db7f951b95ff
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-04-07 12:51:24 +1000

    Add `AveragedCollection` struct and methods

--- 17/object-oriented-characteristics/src/lib.rs
+++ 17/object-oriented-characteristics/src/lib.rs
@@ -1,14 +1,31 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub struct AveragedCollection {
+    list: Vec<i32>,
+    average: f64,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl AveragedCollection {
+    pub fn add(&mut self, value: i32) {
+        self.list.push(value);
+        self.update_average();
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    pub fn remove(&mut self) -> Option<i32> {
+        let result = self.list.pop();
+        match result {
+            Some(value) => {
+                self.update_average();
+                Some(value)
+            }
+            None => None,
+        }
+    }
+
+    pub fn average(&self) -> f64 {
+        self.average
+    }
+
+    pub fn update_average(&mut self) {
+        let total: i32 = self.list.iter().sum();
+        self.average = total as f64 / self.list.len() as f64;
     }
 }
