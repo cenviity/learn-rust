30de141cd1f1c996ab9123bf42ab4f2d5070ffb5
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-04-07 17:16:22 +1000

    Try to use `String` type that doesn't implement `Draw` trait

--- 17/gui/src/main.rs
+++ 17/gui/src/main.rs
@@ -1,6 +1,6 @@
 #![allow(dead_code)]
 
-use gui::{Button, Draw, Screen};
+use gui::{Draw, Screen};
 
 struct SelectBox {
     width: u32,
@@ -16,22 +16,7 @@ impl Draw for SelectBox {
 
 fn main() {
     let screen = Screen {
-        components: vec![
-            Box::new(SelectBox {
-                width: 75,
-                height: 10,
-                options: vec![
-                    String::from("Yes"),
-                    String::from("Maybe"),
-                    String::from("No"),
-                ],
-            }),
-            Box::new(Button {
-                width: 50,
-                height: 10,
-                label: String::from("OK"),
-            }),
-        ],
+        components: vec![Box::new(String::from("Hi"))],
     };
 
     screen.run();
