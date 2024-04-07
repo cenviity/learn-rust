a9f65d12993be7ce85802d974ab58f214b63c3c7
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-04-08 00:32:54 +1000

    Try to implement `split_at_mut` using only safe Rust

--- 19/unsafe-functions-methods/src/main.rs
+++ 19/unsafe-functions-methods/src/main.rs
@@ -1,10 +1,13 @@
+fn split_at_mut(values: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    let len = values.len();
+
+    assert!(mid <= len);
+
+    (&mut values[..mid], &mut values[mid..])
+}
+
 fn main() {
-    let mut v = vec![1, 2, 3, 4, 5, 6];
+    let mut vector = vec![1, 2, 3, 4, 5, 6];
 
-    let r = &mut v[..];
-
-    let (a, b) = r.split_at_mut(3);
-
-    assert_eq!(a, &mut [1, 2, 3]);
-    assert_eq!(b, &mut [4, 5, 6]);
+    let (left, right) = split_at_mut(&mut vector, 3);
 }
