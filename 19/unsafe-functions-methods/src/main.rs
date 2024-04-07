b63ffa2804f160cdf69ebafdf2f2b39bbaad74d4
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-04-08 00:37:07 +1000

    Implement `split_at_mut` with unsafe code

--- 19/unsafe-functions-methods/src/main.rs
+++ 19/unsafe-functions-methods/src/main.rs
@@ -1,9 +1,17 @@
+use std::slice;
+
 fn split_at_mut(values: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
     let len = values.len();
+    let ptr = values.as_mut_ptr();
 
     assert!(mid <= len);
 
-    (&mut values[..mid], &mut values[mid..])
+    unsafe {
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+        )
+    }
 }
 
 fn main() {
