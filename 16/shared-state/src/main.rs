775f79ae17b254678daec1f86cc8d572a8ba8189
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-04-07 12:15:04 +1000

    Replace `Rc` with `Arc`

--- 16/shared-state/src/main.rs
+++ 16/shared-state/src/main.rs
@@ -1,11 +1,14 @@
-use std::{rc::Rc, sync::Mutex, thread};
+use std::{
+    sync::{Arc, Mutex},
+    thread,
+};
 
 fn main() {
-    let counter = Rc::new(Mutex::new(0));
+    let counter = Arc::new(Mutex::new(0));
     let mut handles = vec![];
 
     for _ in 0..10 {
-        let counter = Rc::clone(&counter);
+        let counter = Arc::clone(&counter);
         let handle = thread::spawn(move || {
             let mut num = counter.lock().unwrap();
 
