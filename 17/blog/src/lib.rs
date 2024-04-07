0fe376c0ba24a86f60bf1d759c0fd41fd6e275b0
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-04-07 17:37:41 +1000

    Add `Post` struct and `new` method and related things

--- 17/blog/src/lib.rs
+++ 17/blog/src/lib.rs
@@ -1,14 +1,19 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub struct Post {
+    state: Option<Box<dyn State>>,
+    content: String,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Post {
+    pub fn new() -> Self {
+        Self {
+            state: Some(Box::new(Draft {})),
+            content: String::new(),
+        }
     }
 }
+
+trait State {}
+
+struct Draft {}
+
+impl State for Draft {}
