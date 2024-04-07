e4ab8a077daf61c6e546fac0e57f0ead721f5ed5
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-04-08 00:29:21 +1000

    Demonstrate the safe `split_at_mut` function

--- 19/unsafe-functions-methods/src/main.rs
+++ 19/unsafe-functions-methods/src/main.rs
@@ -1,7 +1,10 @@
 fn main() {
-    unsafe fn dangerous() {}
+    let mut v = vec![1, 2, 3, 4, 5, 6];
 
-    unsafe {
-        dangerous();
-    }
+    let r = &mut v[..];
+
+    let (a, b) = r.split_at_mut(3);
+
+    assert_eq!(a, &mut [1, 2, 3]);
+    assert_eq!(b, &mut [4, 5, 6]);
 }
