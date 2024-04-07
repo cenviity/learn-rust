84af7ac82a8c22647f4429c5c75b62167a966a78
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-04-07 17:19:26 +1000

    Try to extract `components` variable

--- 17/gui/src/main.rs
+++ 17/gui/src/main.rs
@@ -15,24 +15,23 @@ impl Draw for SelectBox {
 }
 
 fn main() {
-    let screen = Screen {
-        components: vec![
-            Box::new(SelectBox {
-                width: 75,
-                height: 10,
-                options: vec![
-                    String::from("Yes"),
-                    String::from("Maybe"),
-                    String::from("No"),
-                ],
-            }),
-            Box::new(Button {
-                width: 50,
-                height: 10,
-                label: String::from("OK"),
-            }),
-        ],
-    };
+    let components = vec![
+        Box::new(SelectBox {
+            width: 75,
+            height: 10,
+            options: vec![
+                String::from("Yes"),
+                String::from("Maybe"),
+                String::from("No"),
+            ],
+        }),
+        Box::new(Button {
+            width: 50,
+            height: 10,
+            label: String::from("OK"),
+        }),
+    ];
+    let screen = Screen { components };
 
     screen.run();
 }
