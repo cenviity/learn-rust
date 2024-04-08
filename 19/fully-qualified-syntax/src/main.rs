d5460b24ecfe4f87a2c46f903f08a8ef4448feee
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-04-08 21:54:44 +1000

    Add `Human` struct and two traits all with a `fly` method each

--- 19/fully-qualified-syntax/src/main.rs
+++ 19/fully-qualified-syntax/src/main.rs
@@ -1,3 +1,32 @@
+trait Pilot {
+    fn fly(&self);
+}
+
+trait Wizard {
+    fn fly(&self);
+}
+
+struct Human;
+
+impl Human {
+    fn fly(&self) {
+        println!("*waving arms furiously*");
+    }
+}
+
+impl Pilot for Human {
+    fn fly(&self) {
+        println!("This is your captain speaking.");
+    }
+}
+
+impl Wizard for Human {
+    fn fly(&self) {
+        println!("Up!");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let person = Human;
+    person.fly();
 }
