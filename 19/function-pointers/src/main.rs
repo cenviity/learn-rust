12b665f714858fe0c93c18fcc9d5b36ec5fcb7b0
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-04-09 10:17:36 +1000

    Use a function in `map`

--- 19/function-pointers/src/main.rs
+++ 19/function-pointers/src/main.rs
@@ -1,4 +1,4 @@
 fn main() {
     let list_of_numbers = vec![1, 2, 3];
-    let list_of_strings: Vec<String> = list_of_numbers.iter().map(|i| i.to_string()).collect();
+    let list_of_strings: Vec<String> = list_of_numbers.iter().map(ToString::to_string).collect();
 }
