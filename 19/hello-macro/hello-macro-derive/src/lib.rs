2079e3ec5c6d6176cb4c7c1f5ed185cbeb688501
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-04-10 11:27:26 +1000

    Add template for procedural macro

--- 19/hello-macro/hello-macro-derive/src/lib.rs
+++ 19/hello-macro/hello-macro-derive/src/lib.rs
@@ -1,14 +1,13 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use proc_macro::TokenStream;
+use quote::quote;
+use syn;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[proc_macro_derive(HelloMacro)]
+pub fn hello_macro_derive(input: TokenStream) -> TokenStream {
+    // Construct a representation of Rust code as a syntax tree
+    // that we can manipulate
+    let ast = syn::parse(input).unwrap();
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    // Build the trait implementation
+    impl_hello_macro(&ast)
 }
