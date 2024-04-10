3c9812dd0594a2841f4e371aa7cbd574ad753097
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-04-10 16:02:44 +1000

    Panic if supplied `ThreadPool` size is zero

--- 20/hello/src/lib.rs
+++ 20/hello/src/lib.rs
@@ -1,7 +1,16 @@
 pub struct ThreadPool;
 
 impl ThreadPool {
+    /// Create a new `ThreadPool`.
+    ///
+    /// The `size` is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// This function will panic if `size` is zero.
     pub fn new(size: usize) -> Self {
+        assert!(size > 0);
+
         Self
     }
 
