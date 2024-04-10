b2b3173859a2d86c2b47016f816c0200ca0bdd68
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-04-10 16:28:54 +1000

    Update `ThreadPool` to hold `Worker` instances instead

--- 20/hello/src/lib.rs
+++ 20/hello/src/lib.rs
@@ -1,7 +1,12 @@
 use std::thread;
 
 pub struct ThreadPool {
-    threads: Vec<thread::JoinHandle<()>>,
+    workers: Vec<Worker>,
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
 }
 
 impl ThreadPool {
@@ -15,18 +20,26 @@ impl ThreadPool {
     pub fn new(size: usize) -> Self {
         assert!(size > 0);
 
-        let mut threads = Vec::with_capacity(size);
+        let mut workers = Vec::with_capacity(size);
 
-        for _ in 0..size {
-            // create some threads and store them in the vector
+        for id in 0..size {
+            workers.push(Worker::new(id));
         }
 
-        Self { threads }
+        Self { workers }
     }
 
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
+    }
+}
+
+impl Worker {
+    fn new(id: usize) -> Self {
+        let thread = thread::spawn(|| {});
+
+        Self { id, thread }
     }
 }
