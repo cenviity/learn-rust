30257ef2d17f88f749faf077c7c0fa579762d850
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-04-10 15:57:17 +1000

    Add `ThreadPool::execute` method

--- 20/hello/src/lib.rs
+++ 20/hello/src/lib.rs
@@ -4,4 +4,10 @@ impl ThreadPool {
     pub fn new(size: usize) -> Self {
         Self
     }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+    }
 }
