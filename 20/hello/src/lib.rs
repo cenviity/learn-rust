15de85ba118687faae9dca044e38c0d8653e9ef9
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-04-10 16:12:07 +1000

    Try to create threads and store them in `ThreadPool`

--- 20/hello/src/lib.rs
+++ 20/hello/src/lib.rs
@@ -1,4 +1,8 @@
-pub struct ThreadPool;
+use std::thread;
+
+pub struct ThreadPool {
+    threads: Vec<thread::JoinHandle<()>>,
+}
 
 impl ThreadPool {
     /// Create a new `ThreadPool`.
@@ -11,7 +15,13 @@ impl ThreadPool {
     pub fn new(size: usize) -> Self {
         assert!(size > 0);
 
-        Self
+        let mut threads = Vec::with_capacity(size);
+
+        for _ in 0..size {
+            // create some threads and store them in the vector
+        }
+
+        Self { threads }
     }
 
     pub fn execute<F>(&self, f: F)
